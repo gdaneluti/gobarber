4a59bed859b56b4f7c90cab60dbbdb9c620e23f9
Gustavo Daneluti <gustavo.daneluti@ezycollect.com.br>
2019-12-21 22:55:07 -0300

    Loader models to database

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Rocketseat' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Gustavo Daneluti',
+    email: 'gustavo_daneluti@hotmail.com',
+    password_hash: '12345'
+  });
+
+  res.json(user);
+});
 
 export default routes;
